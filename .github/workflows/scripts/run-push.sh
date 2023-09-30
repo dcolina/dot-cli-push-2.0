cac1691916c81fe378cf9429476db480991b17b8
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2023-09-29 21:11:55 -0600

    lol

--- .github/workflows/scripts/run-push.sh
+++ .github/workflows/scripts/run-push.sh
@@ -49,6 +49,11 @@ _setup_CLI(){
     #Lets create the services file dot-service.yml
     #the services yml is used to store the server configurations or profiles if you Will
     DOT_SERVICES_HOME=$HOME/.dotcms/
+    echo "Creating dotCMS CLI home: $DOT_SERVICES_HOME"
+
+    echo " user home "  ~
+    echo whoami
+
     DOT_SERVICE_YML=".dot-service.yml"
     SERVICE_FILE=$DOT_SERVICES_HOME$DOT_SERVICE_YML
   # All we need is a file with an active profile that matches the server we want to connect to in this case we are using default
@@ -76,8 +81,6 @@ cat_log(){
 _run_cli_push(){
       workspace_path=$1
       token=$2
-
-      echo  DEFAULT :::  "$DOTCMS_CLIENT_SERVERS_DEFAULT"
 
       #These environment vars are expected by the start-up script
       export JAVA_OPTIONS="-Dquarkus.http.host=0.0.0.0 -Djava.util.logging.manager=org.jboss.logmanager.LogManager"

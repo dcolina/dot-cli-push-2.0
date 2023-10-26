9ea4bb93acfda65ed67ce59a33f1776de30be2b1
daniel.colina <daniel.colina@dotcms.com>
2023-10-26 14:25:20 +0200

    #26386 testing command help.

--- .github/workflows/scripts/run-push.sh
+++ .github/workflows/scripts/run-push.sh
@@ -95,7 +95,8 @@ _run_cli_push(){
       export JAVA_APP_NAME="dotcms-cli"
       # Log file
       export QUARKUS_LOG_FILE_PATH="$DOT_CLI_HOME"dotcms-cli.log
-      bash /tmp/dot-cli/run-java.sh "push" "$workspace_path" "--removeAssets" "--removeFolders" "--token" "$token" "--errors"
+      bash /tmp/dot-cli/run-java.sh "push" "--help"
+      # bash /tmp/dot-cli/run-java.sh "push" "$workspace_path" "--removeAssets" "--removeFolders" "--token" "$token" "--errors"
       export exit_code=$?
       echo $exit_code
 }
@@ -119,5 +120,5 @@ run_cli_push(){
 }
 
 print_env(){
-  env
+  env | grep -i "DOT_CLI"
 }
